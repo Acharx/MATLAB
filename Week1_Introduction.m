592cfb0d35d43b39b43056681a91ac9035e7713e
Acharx <mehmet.achar@gmail.com>
2021-12-11 00:14:39 +0300

    Matlab

---
+++ Week1_Introduction.m
@@ -0,0 +1,36 @@
+2-3*4
+x=2-3*4
+2*x
+t=5
+t=t+1
+%clear
+%clear all
+%clc
+%clear a
+%format long => shows the typed number in more fractions
+%format short=> shows the typed number in less fractions
+%whos =>shows proporties of variables
+%diary Week1_Introduction (?)
+pi %already defined constant 3.14...
+
+%   Write to commend line
+%   >>format short
+%   >>x=-491/3
+%   x =
+%      -163.6667 
+
+%   >>format long
+%   >>x=-491/3
+%   x =
+%      -1.636666666666667e+02
+
+%ENTERING MULTIPLE STATEMENT
+a = 7; b=cos(a), c=cosh(a)
+% Result will be;
+% b =
+%    0.6570
+% c = 
+%   548.3170
+
+
+
